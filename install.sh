2bbace7b34e7adc54ee686fcfa9e325f7edb807b
suzalshrestha <sujalshresthawork@gmail.com>
2026-03-07 13:02:30 +0545

    add cask and brew

---
+++ install.sh
@@ -0,0 +1,11 @@
+t clone https://github.com/suzalshrestha/.config
+cd .config
+brew bundle
+
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.Dock autohide -bool true
+defaults write com.apple.Dock autohide-delay -float 1000
+defaults write com.apple.universalaccess reduceMotion -bool true
+
+killall SystemUIServer
+killall Dock
